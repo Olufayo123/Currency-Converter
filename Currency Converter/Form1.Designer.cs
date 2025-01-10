1e194d1d1ecb0419140cbc01c682159fa26aefcb
victor-olufayo <victorolufayo@tekexperts571.onmicrosoft.com>
2025-01-10 15:05:53 +0100

    Initial commit

---
+++ Currency Converter/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Currency_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SourceCombo = new MetroFramework.Controls.MetroComboBox();
+            this.ConvertedCombo = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.txtAmount = new MetroFramework.Controls.MetroTextBox();
+            this.TextResult = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(158, 423);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(61, 26);
+            this.metroButton2.TabIndex = 0;
+            this.metroButton2.Text = "Convert";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // SourceCombo
+            // 
+            this.SourceCombo.FormattingEnabled = true;
+            this.SourceCombo.ItemHeight = 23;
+            this.SourceCombo.Location = new System.Drawing.Point(156, 63);
+            this.SourceCombo.Name = "SourceCombo";
+            this.SourceCombo.Size = new System.Drawing.Size(121, 29);
+            this.SourceCombo.TabIndex = 1;
+            this.SourceCombo.UseSelectable = true;
+            // 
+            // ConvertedCombo
+            // 
+            this.ConvertedCombo.FormattingEnabled = true;
+            this.ConvertedCombo.ItemHeight = 23;
+            this.ConvertedCombo.Location = new System.Drawing.Point(156, 114);
+            this.ConvertedCombo.Name = "ConvertedCombo";
+            this.ConvertedCombo.Size = new System.Drawing.Size(121, 29);
+            this.ConvertedCombo.TabIndex = 2;
+            this.ConvertedCombo.UseSelectable = true;
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(101, 63);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(49, 19);
+            this.metroLabel5.TabIndex = 3;
+            this.metroLabel5.Text = "Source";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(67, 114);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(71, 19);
+            this.metroLabel6.TabIndex = 4;
+            this.metroLabel6.Text = "Converted";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(94, 168);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(56, 19);
+            this.metroLabel7.TabIndex = 5;
+            this.metroLabel7.Text = "Amount";
+            // 
+            // txtAmount
+            // 
+            // 
+            // 
+            // 
+            this.txtAmount.CustomButton.Image = null;
+            this.txtAmount.CustomButton.Location = new System.Drawing.Point(64, 1);
+            this.txtAmount.CustomButton.Name = "";
+            this.txtAmount.CustomButton.Size = new System.Drawing.Size(19, 19);
+            this.txtAmount.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtAmount.CustomButton.TabIndex = 1;
+            this.txtAmount.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtAmount.CustomButton.UseSelectable = true;
+            this.txtAmount.CustomButton.Visible = false;
+            this.txtAmount.Lines = new string[0];
+            this.txtAmount.Location = new System.Drawing.Point(158, 168);
+            this.txtAmount.MaxLength = 32767;
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.PasswordChar = '\0';
+            this.txtAmount.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtAmount.SelectedText = "";
+            this.txtAmount.SelectionLength = 0;
+            this.txtAmount.SelectionStart = 0;
+            this.txtAmount.ShortcutsEnabled = true;
+            this.txtAmount.Size = new System.Drawing.Size(84, 21);
+            this.txtAmount.TabIndex = 7;
+            this.txtAmount.UseSelectable = true;
+            this.txtAmount.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtAmount.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // TextResult
+            // 
+            this.TextResult.AutoSize = true;
+            this.TextResult.Location = new System.Drawing.Point(94, 216);
+            this.TextResult.Name = "TextResult";
+            this.TextResult.Size = new System.Drawing.Size(43, 19);
+            this.TextResult.TabIndex = 9;
+            this.TextResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(357, 517);
+            this.Controls.Add(this.TextResult);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.metroLabel7);
+            this.Controls.Add(this.metroLabel6);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.ConvertedCombo);
+            this.Controls.Add(this.SourceCombo);
+            this.Controls.Add(this.metroButton2);
+            this.Name = "Form1";
+            this.Text = "Currency Converter ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroComboBox SourceCombo;
+        private MetroFramework.Controls.MetroComboBox ConvertedCombo;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroTextBox txtAmount;
+        private MetroFramework.Controls.MetroLabel TextResult;
+    }
+}
+
---
+++ Currency Converter/Form1.cs
@@ -0,0 +1,72 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace Currency_Converter
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private Dictionary<string, double> _currencies = new Dictionary<string, double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var currencies = GetLatestCurrrencies();
+            dynamic responseObject = JsonConvert.DeserializeObject(currencies);
+
+            foreach (var item in responseObject.data)
+            {
+                SourceCombo.Items.Add(item.First.code.ToString());
+                ConvertedCombo.Items.Add(item.First.code.ToString());
+                _currencies.Add(item.First.code.ToString(), item.First.value.Value);
+            }
+
+
+        }
+
+        private string GetLatestCurrrencies()
+        {
+            var client = new RestClient("https://api.currencyapi.com/v3/latest");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("apikey", "cur_live_AepvhN1R1ehxyRFSlpRbb4KAtK6D86oXkBHm454S");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return response.Content;
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            var selectedSourceCurrency = SourceCombo.SelectedItem;
+            var selectedConvertedCurrency = ConvertedCombo.SelectedItem;
+            var amount = double.Parse(txtAmount.Text);
+
+            var sourceCurrentExchangeRate = _currencies
+                .First (c=> c.Key == selectedSourceCurrency)
+            .Value;
+
+            var convertedCurrencyExchangeRate = _currencies.
+                First(c => c.Key == selectedConvertedCurrency)
+            .Value;
+
+            var calculatedAmount = (amount * sourceCurrentExchangeRate) * convertedCurrencyExchangeRate;
+
+            TextResult.Text = $"Result : {calculatedAmount} {_currencies.First(c => c.Key == selectedConvertedCurrency).Key}" ;
+
+        }
+
+        
+    }
+        }
+
+     
